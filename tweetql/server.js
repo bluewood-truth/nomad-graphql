60ea2b3d3a86e7c1e637829497f06af3c4d97a97
bluewood <bluewood.truth@gmail.com>
2022-06-06 13:56:12 +0900

    add resolvers

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,22 +1,46 @@
 import {ApolloServer, gql} from 'apollo-server';
 
+let tweets = [
+  {
+    id: '1',
+    text: 'hello',
+  },
+  {
+    id: '2',
+    text: 'hello',
+  },
+  {
+    id: '3',
+    text: 'hello',
+  },
+];
+
+let users = [
+  {
+    id: '1',
+    firstName: 'Hanseul',
+    lastName: 'Kim',
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
     firstName: String!
     lastName: String!
+    fullName: String!
   }
 
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
 
   type Query {
     allTweets: [Tweet!]! # GET /api/v1/tweets
     tweet(id: ID!): Tweet # GET /api/v1/tweets/:id
+    allUsers: [User!]!
   }
 
   type Mutation {
@@ -25,7 +49,43 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({typeDefs});
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    // 항상 첫번째 인수는 root, 두번째 인수는 요청의 arguments
+    tweet(_, {id}) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+    allUsers() {
+      return users;
+    }
+  },
+  Mutation: {
+    postTweet(_, {text}) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, {id}) {
+      const toDelete = tweets.find((tweet) => tweet.id === id);
+      if (!toDelete) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({firstName, lastName}) {
+      return `${firstName} ${lastName}`;
+    }
+  }
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
 
 server.listen().then(({url}) => {
   console.log(`Running on ${url}`);
