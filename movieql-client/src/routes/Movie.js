a9852d4f075fdda9589f1fc2a78fefafa6b9478c
bluewood <bluewood.truth@gmail.com>
2022-06-08 20:39:40 +0900

    add movie details page

--- movieql-client/src/routes/Movie.js
+++ movieql-client/src/routes/Movie.js
@@ -1,5 +1,34 @@
+import {gql, useQuery} from '@apollo/client';
+import {useParams} from 'react-router-dom';
+
+const MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+    }
+  }
+`;
+
 const Movie = () => {
-  return <div>This is movie details.</div>;
+  const {id} = useParams();
+  const {data, loading, error} = useQuery(MOVIE, {
+    variables: {id: parseInt(id)},
+  });
+
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>cannot fetch :(</h1>;
+  }
+
+  return (
+    <div>
+      <h1>{data.movie.title}</h1>
+    </div>
+  );
 };
 
 export default Movie;
