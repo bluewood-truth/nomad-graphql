961a76e8644e91cd09594be77eec2dc63ba0a48f
bluewood <bluewood.truth@gmail.com>
2022-06-08 20:11:45 +0900

    add list of movies page

--- movieql-client/src/routes/Movies.js
+++ movieql-client/src/routes/Movies.js
@@ -1,5 +1,35 @@
+import {gql, useQuery} from '@apollo/client';
+import { Link } from 'react-router-dom';
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      title
+      id
+    }
+  }
+`;
+
 const Movies = () => {
-  return <div>This is a list of movies.</div>;
+  const {data, loading, error} = useQuery(ALL_MOVIES);
+
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>cannot fetch :(</h1>
+  }
+
+  return (
+    <ul>
+      {data.allMovies.map((movie) => (
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Movies;
